b4486442913dfe000358b454b6869909a3e32bec
eichi.matsui <skent_aqua@yahoo.co.jp>
2023-02-17 16:28:45 +0900

    定数ファイルを作成

---
+++ src/main/java/constants/AttributeConst.java
@@ -0,0 +1,68 @@
+package constants;
+
+/**
+ * 画面の項目値等を定義するEnumクラス
+ *
+ */
+public enum AttributeConst {
+
+    //フラッシュメッセージ
+    FLUSH("flush"),
+
+    //一覧画面共通
+    MAX_ROW("maxRow"),
+    PAGE("page"),
+
+    //入力フォーム共通
+    TOKEN("_token"),
+    ERR("errors"),
+
+    //ログイン画面
+    LOGIN_ERR("loginError"),
+
+    //利用者管理
+    POST("post"),
+    POSTS("posts"),
+    POS_COUNT("posts_count"),
+    POS_ID("id"),
+    POS_PASS("password"),
+    POS_NAME("name"),
+
+    //トピック管理
+    TOPIC("topic"),
+    TOPICS("topics"),
+    TOP_COUNT("topics_count"),
+    TOP_ID("id"),
+    TOP_TITLE("title"),
+
+    //コメント管理
+    COMMENT("comment"),
+    COMMENTS("comments"),
+    COM_COUNT("comments_count"),
+    COM_ID("id"),
+    COM_DATETIME("comment_datetime"),
+    COM_MSG("msg"),
+    COM_like("like");
+
+    private final String text;
+    private final Integer i;
+
+    private AttributeConst(final String text) {
+        this.text = text;
+        this.i = null;
+    }
+
+    private AttributeConst(final Integer i) {
+        this.text = null;
+        this.i = i;
+    }
+
+    public String getValue() {
+        return this.text;
+    }
+
+    public Integer getIntegerValue() {
+        return this.i;
+    }
+
+}
---
+++ src/main/java/constants/JpaConst.java
@@ -0,0 +1,70 @@
+package constants;
+
+/**
+ * DB関連の項目値を定義するインターフェース
+ * ※インターフェイスに定義した変数は public static final 修飾子がついているとみなされる
+ */
+public interface JpaConst {
+
+    //persistence-unit名
+    String PERSISTENCE_UNIT_NAME = "bulletin_board";
+
+    //トピックデータ取得件数の最大値
+    int ROW_PER_PAGE_TOPIC = 15; //1ページに表示するレコードの数
+    //コメントデータ取得件数の最大値
+    int ROW_PER_PAGE_COMMENT = 50; //1ページに表示するレコードの数
+
+    //利用者テーブル
+    String TABLE_POS = "posts"; //テーブル名
+    //利用者テーブルカラム
+    String POS_COL_ID = "id"; //id
+    String POS_COL_NAME = "name"; //名前
+    String POS_COL_PASS = "password"; //パスワード
+    String POS_COL_CREATED_AT = "created_at"; //登録日時
+
+    //トピックテーブル
+    String TABLE_TOP = "topics"; //テーブル名
+    //トピックテーブルカラム
+    String TOP_COL_ID = "id"; //id
+    String TOP_COL_POS = "post_id"; //トピックを作成した利用者のid
+    String TOP_COL_TITLE = "title"; //トピックのタイトル
+    String TOP_COL_CREATED_AT = "created_at"; //作成日時
+
+    //コメントテーブル
+    String TABLE_COM = "comments"; //テーブル名
+    //コメントテーブルカラム
+    String COM_COL_ID = "id"; //id
+    String COM_COL_POS = "post_id"; //コメントを作成した利用者のid
+    String COM_COL_TOP = "topic_id"; //コメントされたトピックのid
+    String COM_COL_MSG  = "msg"; //コメントの内容
+    String COM_COL_CREATED_AT = "created_at"; //作成日時
+
+    //Entity名
+    String ENTITY_POS = "post"; //利用者
+    String ENTITY_TOP = "topic"; //トピック
+    String ENTITY_COM = "comment"; //コメント
+
+    //JPQL内パラメータ
+    String JPQL_PARM_PASSWORD = "password"; //パスワード
+    String JPQL_PARM_POST = "post"; //利用者
+
+    //NamedQueryの nameとquery
+    //全ての利用者をidの降順に取得する
+    String Q_POS_GET_ALL = ENTITY_POS + ".getAll"; //name
+    String Q_POS_GET_ALL_DEF = "SELECT e FROM Post AS e ORDER BY e.id DESC"; //query
+    //全ての利用者の件数を取得する
+    String Q_POS_COUNT = ENTITY_POS + ".count";
+    String Q_POS_COUNT_DEF = "SELECT COUNT(e) FROM Post AS e";
+    //全てのトピックをidの降順に取得する
+    String Q_TOP_GET_ALL = ENTITY_TOP + ".getAll";
+    String Q_TOP_GET_ALL_DEF = "SELECT r FROM Topic AS r ORDER BY r.id DESC";
+    //全てのトピックの件数を取得する
+    String Q_TOP_COUNT = ENTITY_TOP + ".count";
+    String Q_TOP_COUNT_DEF = "SELECT COUNT(r) FROM Topic AS r";
+    //全てのコメントをidの降順に取得する
+    String Q_COM_GET_ALL = ENTITY_COM + ".getAll";
+    String Q_COM_GET_ALL_DEF = "SELECT r FROM Comment AS r ORDER BY r.id DESC";
+    //全てのコメントの件数を取得する
+    String Q_COM_COUNT = ENTITY_COM + ".count";
+    String Q_COM_COUNT_DEF = "SELECT COUNT(r) FROM Comment AS r";
+}
