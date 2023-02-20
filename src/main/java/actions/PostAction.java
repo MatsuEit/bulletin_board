2d5e2541d34ca2e75bb04fc3d30ccf0059511776
eichi.matsui <skent_aqua@yahoo.co.jp>
2023-02-21 01:56:32 +0900

    利用者createアクションの作成

--- src/main/java/actions/PostAction.java
+++ src/main/java/actions/PostAction.java
@@ -8,6 +8,8 @@ import javax.servlet.ServletException;
 import actions.views.PostView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.MessageConst;
+import constants.PropertyConst;
 import services.PostService;
 
 /**
@@ -68,6 +70,52 @@ public class PostAction extends ActionBase {
 
         //新規登録画面を表示
         forward(ForwardConst.FW_POS_NEW);
+    }
+
+    /**
+     * 新規登録を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void create() throws ServletException, IOException {
+
+        //CSRF対策 tokenのチェック
+        if (checkToken()) {
+
+            //パラメータの値を元に従業員情報のインスタンスを作成する
+            PostView ev = new PostView(
+                    null,
+                    getRequestParam(AttributeConst.POS_NAME),
+                    getRequestParam(AttributeConst.POS_PASS),
+                    null);
+
+            //アプリケーションスコープからpepper文字列を取得
+            String pepper = getContextScope(PropertyConst.PEPPER);
+
+            //従業員情報登録
+            List<String> errors = service.create(ev, pepper);
+
+            if (errors.size() > 0) {
+                //登録中にエラーがあった場合
+
+                putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+                putRequestScope(AttributeConst.POST, ev); //入力された利用者情報
+                putRequestScope(AttributeConst.ERR, errors); //エラーのリスト
+
+                //新規登録画面を再表示
+                forward(ForwardConst.FW_POS_NEW);
+
+            } else {
+                //登録中にエラーがなかった場合
+
+                //セッションに登録完了のフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
+
+                //一覧画面にリダイレクト
+                redirect(ForwardConst.ACT_POS, ForwardConst.CMD_INDEX);
+            }
+
+        }
     }
 
 }
